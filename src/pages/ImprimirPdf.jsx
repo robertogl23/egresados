f9802922da2b8cd5d6186b774bc08932c2418af5
Ramses <ecologiaramses62@gmail.com>
2021-03-11 20:17:02 -0600

    cambios en archivo de pdf y en el boton de pdf

--- src/pages/ImprimirPdf.jsx
+++ src/pages/ImprimirPdf.jsx
@@ -1,24 +1,65 @@
 import React, { useEffect } from 'react'
 import GridEstadisticas1 from '../components/dashboardGrid/GridEstadisticas1';
+import LOGO_TESI from '../img/LOGO_TESI.png';
+import logodelestado from '../img/logodelestado.jpg';
+import styled from 'styled-components';
+const ImprimirPdfStyled = styled.div`
+display: flex;
+justify-content: space-between;
+width: 100%;
+ .logo1 {
 
+    height: 100px;
+    width: 200px;
+}
+ .logo2 {
+
+     height: 100px;
+    width: 200px;
+}
+img {
+    height: auto;
+    width:100%;
+}
+button{
+    height: 30px;
+    width: 30px;
+}
+.boton {
+    display: block;
+}
+.flex {
+    display: flex;
+    flex-direction: row;
+}
+`;
 const ImprimirPdf = () => {
 
     useEffect(() => {
-    
-      console.log(window.document.querySelector('header').style.display = "none");
-      console.log(window.document.getElementById('section-egresados').style.display = "none");
-      window.document.querySelector('section').style.padding = '0'
-    },[])
+
+        console.log(window.document.querySelector('header').style.display = "none");
+        console.log(window.document.getElementById('section-egresados').style.display = "none");
+        window.document.querySelector('section').style.padding = '0'
+    }, [])
 
     const ImprimirPdf = () => {
         window.document.querySelector('button').style.display = "none"
         window.print()
     }
     return (
-        <div className="centrado">
-            <h1>Hola</h1>
-          
+        <div className="centrado flex">
+            < ImprimirPdfStyled  >
+                <div className='logo1'>
+                    <img src={LOGO_TESI} />
+                </div>
+                <div className='logo2'>
+                    <img src={logodelestado} />
+                </div>
+            </ ImprimirPdfStyled >
+            
+            <div className='boton'>
             <button onClick={ImprimirPdf}>pdf</button>
+            </div>
         </div>
     )
 }
