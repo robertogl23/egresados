0339efd95ea1e2f0630a7058cf71ee75c37800b7
roberto <dev.robertgl@gmail.com>
2021-03-11 22:58:30 -0600

    arreglando diseño pdf

--- src/pages/ImprimirPdf.jsx
+++ src/pages/ImprimirPdf.jsx
@@ -1,67 +1,93 @@
-import React, { useEffect } from 'react'
-import GridEstadisticas1 from '../components/dashboardGrid/GridEstadisticas1';
-import LOGO_TESI from '../img/LOGO_TESI.png';
-import logodelestado from '../img/logodelestado.jpg';
-import styled from 'styled-components';
+import React, { useEffect } from "react";
+import GridEstadisticas1 from "../components/dashboardGrid/GridEstadisticas1";
+import LOGO_TESI from "../img/LOGO_TESI.png";
+import logodelestado from "../img/logodelestado.jpg";
+import styled from "styled-components";
+import { Grid } from "../components/grid/Grid";
 const ImprimirPdfStyled = styled.div`
-display: flex;
-justify-content: space-between;
-width: 100%;
- .logo1 {
-
-    height: 100px;
-    width: 200px;
-}
- .logo2 {
-
-     height: 100px;
-    width: 200px;
-}
-img {
-    height: auto;
-    width:100%;
-}
-button{
-    height: 30px;
-    width: 30px;
-}
-.boton {
-    display: block;
-}
-.flex {
-    display: flex;
-    flex-direction: row;
-}
+	display: flex;
+	justify-content: space-between;
+	width: 100%;
+	.logo1 {
+		height: 100px;
+		width: 200px;
+	}
+	.logo2 {
+		height: 100px;
+		width: 200px;
+	}
+	img {
+		height: auto;
+		width: 100%;
+	}
+	button {
+		height: 30px;
+		width: 30px;
+	}
+	.boton {
+		display: block;
+	}
+	.flex {
+		display: flex;
+		flex-direction: column;
+        height:100%;
+	}
+    
 `;
 const ImprimirPdf = () => {
+	useEffect(() => {
+		console.log(
+			(window.document.querySelector(
+				"header"
+			).style.display = "none")
+		);
+		console.log(
+			(window.document.getElementById(
+				"section-egresados"
+			).style.display = "none")
+		);
+		window.document.querySelector("section").style.padding =
+			"0";
+	}, []);
 
-    useEffect(() => {
+	const ImprimirPdf = () => {
+		window.document.querySelector("button").style.display =
+			"none";
+		window.print();
+	};
+	return (
+		<div className='flex'>
+			<ImprimirPdfStyled>
+				<div className='logo1'>
+					<img src={LOGO_TESI} />
+				</div>
+				<div className='logo2'>
+					<img src={logodelestado} />
+				</div>
+			</ImprimirPdfStyled>
 
-        console.log(window.document.querySelector('header').style.display = "none");
-        console.log(window.document.getElementById('section-egresados').style.display = "none");
-        window.document.querySelector('section').style.padding = '0'
-    }, [])
+			<div className='boton'>
+				<button onClick={ImprimirPdf}>pdf</button>
+			</div>
+            <div className="cont-grid">
 
-    const ImprimirPdf = () => {
-        window.document.querySelector('button').style.display = "none"
-        window.print()
-    }
-    return (
-        <div className="centrado flex">
-            < ImprimirPdfStyled  >
-                <div className='logo1'>
-                    <img src={LOGO_TESI} />
-                </div>
-                <div className='logo2'>
-                    <img src={logodelestado} />
-                </div>
-            </ ImprimirPdfStyled >
-            
-            <div className='boton'>
-            <button onClick={ImprimirPdf}>pdf</button>
             </div>
-        </div>
-    )
-}
+            <ImprimirPdfStyled>
+				<div className='logo1'>
+					<img src={LOGO_TESI} />
+				</div>
+				<div className='logo2'>
+					<img src={logodelestado} />
+				</div>
+			</ImprimirPdfStyled>
+            <div className="cont-grid">
 
-export default ImprimirPdf
+            </div>
+            <div className="cont-grid">
+
+            </div>
+		</div>
+	);
+};
+
+export default ImprimirPdf;
